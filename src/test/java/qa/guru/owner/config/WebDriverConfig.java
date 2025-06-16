1fff61a2fd08c11a420651259a6f8d63db1499fc
Sergey Dubinin <s.dubinin@dex-it.ru>
2025-06-16 17:06:27 +0300

    add remote URL

--- src/test/java/qa/guru/owner/config/WebDriverConfig.java
+++ src/test/java/qa/guru/owner/config/WebDriverConfig.java
@@ -1,6 +1,23 @@
 package qa.guru.owner.config;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Objects;
+
 public class WebDriverConfig {
+
+    public URL getRemoteUrl(){
+        String remoteUrl = System.getProperty("remoteUrl");
+        if (Objects.nonNull(remoteUrl)){
+            try {
+                return new URL(remoteUrl);
+            } catch (MalformedURLException e) {
+                throw new RuntimeException("Bad Url");
+            }
+        } else {
+            return null;
+        }
+    }
 
     public Browser getBrowser(){
         String browserString = System.getProperty("browser", "CHROME");
--- src/test/java/qa/guru/owner/config/WebDriverProvider.java
+++ src/test/java/qa/guru/owner/config/WebDriverProvider.java
@@ -3,7 +3,10 @@ package qa.guru.owner.config;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.util.Objects;
 import java.util.function.Supplier;
 
 public class WebDriverProvider implements Supplier<WebDriver> {
@@ -22,12 +25,20 @@ public class WebDriverProvider implements Supplier<WebDriver> {
     }
 
     private WebDriver createWebDriver(){
-        if (config.getBrowser().equals(Browser.FIREFOX)){
-            return new FirefoxDriver();
+
+        if (Objects.isNull(config.getRemoteUrl())){
+            if (config.getBrowser().equals(Browser.FIREFOX)){
+                return new FirefoxDriver();
+            }
+            if (config.getBrowser().equals(Browser.CHROME)){
+                return new ChromeDriver();
+            }
+        } else {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName("FIREFOX");
+            return new RemoteWebDriver(config.getRemoteUrl(), capabilities);
         }
-        if (config.getBrowser().equals(Browser.CHROME)){
-            return new ChromeDriver();
-        }
+
         throw new NullPointerException("No such driver");
     }
 
