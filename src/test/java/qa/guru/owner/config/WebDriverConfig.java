a6bc011ddce9155f7d8aa0bdf7a62512a18f89b0
Sergey Dubinin <s.dubinin@dex-it.ru>
2025-06-16 17:06:06 +0300

    add default params

--- src/test/java/qa/guru/owner/config/WebDriverConfig.java
+++ src/test/java/qa/guru/owner/config/WebDriverConfig.java
@@ -3,12 +3,12 @@ package qa.guru.owner.config;
 public class WebDriverConfig {
 
     public Browser getBrowser(){
-        String browserString = System.getProperty("browser");
+        String browserString = System.getProperty("browser", "CHROME");
         return Browser.valueOf(browserString);
     }
 
     public String getBaseUrl(){
-        return System.getProperty("baseUrl");
+        return System.getProperty("baseUrl", "https://github.com/");
     }
 
 }
