109714c09d4ca91bc6315fb38518a05e23e7ced6
Sergey Dubinin <s.dubinin@dex-it.ru>
2025-06-17 14:50:29 +0300

    fix logging

--- src/test/java/qa/guru/owner/CredentialsTest.java
+++ src/test/java/qa/guru/owner/CredentialsTest.java
@@ -2,9 +2,13 @@ package qa.guru.owner;
 
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import qa.guru.owner.credentialsConfig.CredentialsConfig;
 
 public class CredentialsTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CredentialsTest.class);
 
     @Test
     void credentialsTest() {
@@ -14,6 +18,6 @@ public class CredentialsTest {
 
         String message = String.format("Log in with '%s' login and '%s' password", login, password);
 
-        System.out.println(message);
+        LOGGER.info(message);
     }
 }
